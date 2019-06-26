1a7a02eda347ecdf8928566692b3e915f1432088
Man H Tsui <mantsui@ucdavis.edu>
2019-06-25 18:54:25 -0700

    Updates

---
+++ precache-manifest.d27a122f6856949cb58b1fbff3503f20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09688fa88ac747ccc3b3c21e2d27a3c5",
+    "url": "/react-test-10/index.html"
+  },
+  {
+    "revision": "d6b493027b606bf95c24",
+    "url": "/react-test-10/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a0c583b4c2480847b39d",
+    "url": "/react-test-10/static/js/2.96809de7.chunk.js"
+  },
+  {
+    "revision": "d6b493027b606bf95c24",
+    "url": "/react-test-10/static/js/main.0401d933.chunk.js"
+  },
+  {
+    "revision": "046b0c0518bbc0fad825",
+    "url": "/react-test-10/static/js/runtime~main.4da686ae.js"
+  }
+]);
